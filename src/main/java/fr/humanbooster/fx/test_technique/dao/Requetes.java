c367ea0dbf29afd43935aeb6f58a5c701e83cf32
Gregoryhumanbooster <62284530+Gregoryhumanbooster@users.noreply.github.com>
2020-06-29 11:44:40 +0200

    Update Requetes.java

--- src/main/java/fr/humanbooster/fx/test_technique/dao/Requetes.java
+++ src/main/java/fr/humanbooster/fx/test_technique/dao/Requetes.java
@@ -9,16 +9,19 @@ public class Requetes {
 	public static final String UPDATE_UTILISATEUR_MOT_DE_PASSE = "UPDATE utilisateur SET motDePasse=? WHERE id=?";
 	public static final String SUPPRESSION_UTILISATEUR = "DELETE FROM utilisateur WHERE id=?";
 
+	// FORMATION
 	public static final String AJOUT_FORMATION = "INSERT INTO formation (intitule, description) VALUES (?, ?)";
 	public static final String FORMATION_PAR_ID = "SELECT id, intitule, description FROM formation WHERE id=?";
 	public static final String TOUTES_LES_FORMATIONS = "SELECT id, intitule, description FROM utilisateur";
 	public static final String SUPPRESSION_FORMATION = "DELETE FROM formation WHERE id=?";
 
+	// STAGIAIRE
 	public static final String AJOUT_STAGIAIRE = "INSERT INTO stagiaire (dateNaissance, numPoleEmploi, estHandicape, idUtilisateur) VALUES (?, ?, ?, ?)";
 	public static final String STAGIAIRE_PAR_ID = "SELECT id, dateNaissance, numPoleEmploi, estHandicape FROM stagiaire WHERE id=?";
 	public static final String TOUS_LES_STAGIAIRES = "SELECT id, dateNaissance, numPoleEmploi, estHandicape FROM stagiaire";
 	public static final String SUPPRESSION_STAGIAIRE = "DELETE FROM stagiaire WHERE id=?";
 
+	// CHEF DE PROJET
 	public static final String AJOUT_CHEF_PROJET = null;
 
 	// DOMAINE
