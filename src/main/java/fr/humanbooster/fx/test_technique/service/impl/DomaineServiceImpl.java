0577b03e0010bcde3f692f0bfa3cd6c942a95848
Gregoryhumanbooster <62284530+Gregoryhumanbooster@users.noreply.github.com>
2020-06-29 11:58:11 +0200

    Create DomaineServiceImpl.java

---
+++ src/main/java/fr/humanbooster/fx/test_technique/service/impl/DomaineServiceImpl.java
@@ -0,0 +1,41 @@
+package fr.humanbooster.fx.test_technique.service.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import fr.humanbooster.fx.test_technique.business.Domaine;
+import fr.humanbooster.fx.test_technique.dao.DomaineDao;
+
+public class DomaineServiceImpl implements DomaineDao {
+
+	@Override
+	public Domaine create(Domaine domaine) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Domaine findOne(Long id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Domaine> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Domaine update(Domaine domaine) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Long id) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
